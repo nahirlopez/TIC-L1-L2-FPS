0b66ba76c7a26151b2d845fce6857f125605da58
nahirlopez <101569242+nahirlopez@users.noreply.github.com>
2022-09-06 14:21:44 -0300

    parte1 TPJERO

---
+++ Assets/Scripts/ENCONTRAR.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ENCONTRAR : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public GameObject lista;
+    bool inv = true;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            lista.SetActive(inv);
+            inv = !inv;
+        }
+
+    }
+
+
+}
